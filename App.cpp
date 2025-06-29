e8928bddfe03589e0c8e263bd3b3cb12921c0d42
DesignCraftsMan <fzoussfr@gmail.com>
2025-06-29 14:20:00 +0100

    created the board (green,white), and added logic to display pieces (pawns) depending on their type

--- App.cpp
+++ App.cpp
@@ -6,6 +6,9 @@ wxIMPLEMENT_APP(App);
 
 
 bool App::OnInit() {
+	wxImage::AddHandler(new wxPNGHandler());
+	wxImage::AddHandler(new wxJPEGHandler());
+	wxImage::AddHandler(new wxBMPHandler());
 	MainFrame* mainFrame = new MainFrame("Chess Game");
 	mainFrame->SetClientSize(900, 600);
 	mainFrame->Center();
